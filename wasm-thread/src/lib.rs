84f441a00dfd975a22918a07f8272887d483b536
IWANABETHATGUY <974153916@qq.com>
2021-04-07 17:25:32 +0800

    feat: 🎸 add fibonacci parallel demo

--- wasm-thread/src/lib.rs
+++ wasm-thread/src/lib.rs
@@ -10,7 +10,23 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-pub fn sort_array_concurrent(arr: &mut [i32])  {
+pub fn sort_array_concurrent(arr: &mut [i32]) {
     arr.par_sort_unstable()
+}
+#[wasm_bindgen]
+pub fn fibonacci_join(n: u32) -> u32 {
+    if n < 20 {
+        return fibonacci(n);
+    }
+    let (a, b) = rayon::join(|| fibonacci_join(n - 1), || fibonacci_join(n - 2));
+    return a + b;
+}
+#[wasm_bindgen]
+pub fn fibonacci(n: u32) -> u32 {
+    match n {
+        1 => 1,
+        0 => 0,
+        _ => fibonacci(n - 1) + fibonacci(n - 2),
+    }
 }
 pub use wasm_bindgen_rayon::init_thread_pool;
--- wasm-thread/www/index.js
+++ wasm-thread/www/index.js
@@ -2,16 +2,31 @@ import * as wasm from "../pkg/wasm_thread";
 
 (async () => {
   await wasm.default();
-  await wasm.initThreadPool(navigator.hardwareConcurrency - 2);
+  await wasm.initThreadPool(navigator.hardwareConcurrency / 2 - 1);
   let arr = new Array(5000000).fill(1);
   for (let i = 0; i < arr.length; i++) {
     arr[i] = Math.round(Math.random() * 10000);
   }
   let arrI32 = new Int32Array(arr);
-  console.time("js");
-  arr.sort()
-  console.timeEnd("js");
-  console.time("wasm");
-  console.log(wasm.sort_array_concurrent(arrI32));
-  console.timeEnd("wasm");
+  // arr.sort()
+  // console.log(wasm.sort_array_concurrent(arrI32));
+
+  // fibonacci bench
+  console.time("js_fib");
+  const js_fib = fibonacci(35);
+  console.timeEnd("js_fib");
+  console.time("wasm_parallel_fib");
+  const wasm_parallel_fib = wasm.fibonacci_join(35);
+  console.timeEnd("wasm_parallel_fib");
+  console.time("wasm")
+  const wasm_fib = wasm.fibonacci(35)
+  console.timeEnd("wasm")
+  console.assert(js_fib === wasm_parallel_fib && js_fib === wasm_fib)
 })();
+
+function fibonacci(n) {
+  if (n < 2) {
+    return n;
+  }
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
